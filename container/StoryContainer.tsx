734db638cff5fb374ece6af0fdaf24cadbcabe7a
SoYoung210 <ethdud1@gmail.com>
2021-06-16 21:39:14 +0900

    feat: add story name and vertical align fix

--- container/StoryContainer.tsx
+++ container/StoryContainer.tsx
@@ -1,16 +1,47 @@
 import React from 'react';
 import storiesData from '@assets/stories.json';
-import Avatar from '@components/avatar';
 import { Flex } from '@components/util/layout/Flex';
+import { theme } from 'tailwind.config';
+import Gradient from '@components/gradient';
+import Image from '@components/image';
+
+const imageWrapperProps: React.HTMLProps<HTMLImageElement> = {
+  className: 'relative',
+};
 
 const StoryContainer = () => {
   return (
-    <Flex elementType="ol" className="gap-x-20">
+    <Flex elementType="ol" className="flex space-x-24 pt-14 px-8">
       {storiesData.data.map(story => {
         return (
-          <li key={story.id}>
-            <Avatar src={story.profileImage} size={60} />
-          </li>
+          <Flex.CenterVertical
+            elementType="li"
+            key={story.id}
+            direction="column"
+          >
+            <Image.RoundShape
+              wrapperProps={imageWrapperProps}
+              size={60}
+              variants={
+                <div className="absolute -top-6 -left-6">
+                  <Gradient.Circle
+                    size={72}
+                    strokeWidth={2.3}
+                    colors={[
+                      theme.colors.deepBlue['500'],
+                      theme.colors.lightGreen['900'],
+                    ]}
+                  />
+                </div>
+              }
+            >
+              <Image.Source
+                src={story.profileImage}
+                alt="스토리_프로필_이미지"
+              />
+            </Image.RoundShape>
+            <span>{story.name}</span>
+          </Flex.CenterVertical>
         );
       })}
     </Flex>
