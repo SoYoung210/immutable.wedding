768114bdddc630df6366513749691c49830736d5
SoYoung210 <ethdud1@gmail.com>
2021-06-17 23:50:35 +0900

    feat: add feed conents - 1 image, username, description

--- container/FeedContainer.tsx
+++ container/FeedContainer.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import feedData from '@assets/feed.json';
 import { Flex } from '@components/util/layout/Flex';
 import Text from '@components/text';
 import Image from '@components/image';
+import { 스크린_너비 } from 'constants/styles';
+import { 계정_이름, 계정_프로필_사진 } from 'constants/config';
 
 const FeedContainer = () => {
   return (
     <div className="pt-10">
       {feedData.data.map(feed => {
         return (
-          <>
+          <Fragment key={feed.id}>
             <Header />
-          </>
+            <Image width={스크린_너비} height={스크린_너비}>
+              <Image.Source src={feed.imageContents[0]} alt="feed_사진" />
+            </Image>
+
+            <Text component="span" weight="bold" size="sm">
+              {계정_이름}
+            </Text>
+            <Text
+              component="p"
+              className="inline ml-8"
+              weight="light"
+              size="sm"
+            >
+              {feed.description}
+            </Text>
+            <Text weight="extralight" size="sm" color="gray-300">
+              {feed.createdAt}
+            </Text>
+          </Fragment>
         );
       })}
     </div>
@@ -19,15 +39,18 @@ const FeedContainer = () => {
 };
 
 const Header = () => (
-  <Flex>
-    <Image.RoundShape>
-      <Image.Source
-        src="https://avatars.githubusercontent.com/u/17924127?v=4"
-        alt="재엽소영사진"
-      />
+  <Flex.CenterVertical className="py-9 px-8">
+    <Image.RoundShape
+      width={30}
+      height={30}
+      wrapperProps={{ className: 'flex' }}
+    >
+      <Image.Source src={계정_프로필_사진} alt="재엽소영사진" />
     </Image.RoundShape>
-    <Text weight="bold">immutable(soso, jbee)</Text>
-  </Flex>
+    <Text weight="bold" className="ml-6">
+      {계정_이름}
+    </Text>
+  </Flex.CenterVertical>
 );
 
 export default FeedContainer;
