50ecbaa4070b43eb70125a0f7abd36af458c976f
SoYoung210 <ethdud1@gmail.com>
2021-06-20 21:20:19 +0900

    chore: add mergeCss util

--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -3,3 +3,10 @@ import { CSSType } from 'stitches.config';
 export interface CSSProps {
   css?: CSSType;
 }
+
+export function mergeCss(
+  defaultCss: CSSType,
+  optionalCss: CSSType | undefined
+) {
+  return optionalCss == null ? defaultCss : { ...defaultCss, ...optionalCss };
+}
