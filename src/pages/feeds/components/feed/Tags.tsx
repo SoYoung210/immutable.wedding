a1626d8926afd15325d383b2600f29094582e3de
JaeYeopHan <ljyhanll@gmail.com>
2021-06-27 22:35:05 +0900

    feat: add Tags components

---
+++ src/pages/feeds/components/feed/Tags.tsx
@@ -0,0 +1,21 @@
+import Text from '@components/text';
+import { Flex } from '@components/util/layout/Flex';
+import React from 'react';
+
+interface Props {
+  values: string[];
+}
+
+export function Tags({ values }: Props) {
+  return (
+    <Flex css={{ columnGap: '$6', mt: '$4' }} wrap="wrap">
+      {values.map(value => {
+        return (
+          <Text weight="medium" size="base" css={{ color: '$blue500' }}>
+            #{value}
+          </Text>
+        );
+      })}
+    </Flex>
+  );
+}
