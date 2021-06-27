190a895d55d845426d4e3e16345816336a407169
JaeYeopHan <ljyhanll@gmail.com>
2021-06-27 23:13:32 +0900

    fix: change right arrow icon

--- src/pages/feeds/components/feed/icon/ArrowRightIcon.tsx
+++ src/pages/feeds/components/feed/icon/ArrowRightIcon.tsx
@@ -4,7 +4,10 @@ export function ArrowRightIcon() {
   return (
     <Image.Root as="button" css={{ display: 'flex' }}>
       <Image width={24} height={24}>
-        <Image.Source src="/assets/icon/arrow-right.jpg" alt="오른쪽 화살표" />
+        <Image.Source
+          src="/assets/icon/arrow-right--white.png"
+          alt="오른쪽 화살표"
+        />
       </Image>
     </Image.Root>
   );
