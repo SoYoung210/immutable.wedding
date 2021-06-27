64b594392612b5cd7a6451fc7288b11262022404
SoYoung210 <ethdud1@gmail.com>
2021-06-27 21:04:00 +0900

    fix: update toast action

--- src/pages/feeds/components/feed/ToastWrapper.tsx
+++ src/pages/feeds/components/feed/ToastWrapper.tsx
@@ -1,39 +1,41 @@
 import { CSSProps, mergeCss } from '@utils/styles';
 import React, { HTMLAttributes, ReactNode } from 'react';
 import { Box } from '@components/box/Box';
-import { AnimatePresence, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
+import { styled } from 'stitches.config';
 
 interface Props extends CSSProps, HTMLAttributes<HTMLDivElement> {
   children: Exclude<ReactNode, null | boolean | undefined>;
 }
 
+const SDiv = styled(motion.div, {
+  position: 'fixed',
+  left: '50%',
+  transform: 'translateX(-50%)',
+});
+
 export function ToastWrapper({ children, css, ...props }: Props) {
   return (
-    <AnimatePresence>
-      <motion.div
-        style={{ position: 'fixed' }}
-        initial={{ opacity: 0, bottom: -50 }}
-        animate={{ opacity: 1, bottom: 0 }}
-        transition={{ duration: 0.5 }}
-        exit={{ opacity: 0, bottom: -50 }}
-        // layout="position"
+    <SDiv
+      initial={{ opacity: 0, bottom: -50 }}
+      animate={{ opacity: 1, bottom: 50 }}
+      exit={{ opacity: 0, bottom: -50 }}
+    >
+      <Box
+        css={mergeCss(
+          {
+            p: '$16',
+            backgroundColor: 'rgba(65, 65, 65, 0.97);',
+            br: '$4',
+            color: '$white',
+            width: '$520',
+          },
+          css
+        )}
+        {...props}
       >
-        <Box
-          css={mergeCss(
-            {
-              p: '$16',
-              backgroundColor: 'rgba(65, 65, 65, 0.97);',
-              br: '$4',
-              color: '$white',
-              width: '$520',
-            },
-            css
-          )}
-          {...props}
-        >
-          {children}
-        </Box>
-      </motion.div>
-    </AnimatePresence>
+        {children}
+      </Box>
+    </SDiv>
   );
 }
