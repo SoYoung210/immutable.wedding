2aac37957829bda17642667dc7751abfdb9355de
JaeYeopHan <ljyhanll@gmail.com>
2021-06-27 19:02:26 +0900

    feat: add setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,28 @@
+import { NextSeo } from 'next-seo';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import '../styles/globals.css';
+
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <NextSeo title="wedding.log" description="소영과 재엽의 웨딩로그" />
       <Head>
-        <title>wedding.log</title>
-        <meta name="description" content="소영과 재엽의 웨딩로그" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
+      <QueryClientProvider client={client}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </>
   );
 }
