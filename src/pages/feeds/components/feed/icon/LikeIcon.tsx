c9ad20178478d770d1cfa072ba81b2742dcb9b33
SoYoung210 <ethdud1@gmail.com>
2021-06-27 21:04:11 +0900

    fix: like icon handler

--- src/pages/feeds/components/feed/icon/LikeIcon.tsx
+++ src/pages/feeds/components/feed/icon/LikeIcon.tsx
@@ -1,11 +1,41 @@
 import Image from '@components/image';
+import { useNotifications } from '@components/notification/NotificationContext';
+import React, { HTMLAttributes, MouseEvent, useCallback, useMemo } from 'react';
+import { ToastWrapper } from '../ToastWrapper';
 
-export function LikeIcon() {
-  return (
-    <Image.Root as="button">
+type Props = HTMLAttributes<HTMLButtonElement>;
+
+export function LikeIcon({ onClick, ...props }: Props) {
+  const { showNotification } = useNotifications();
+
+  const openToast = useCallback(() => {
+    showNotification({
+      element: (
+        <ToastWrapper>📝 전하고 싶은 말은 식장에서 해주세요!</ToastWrapper>
+      ),
+      autoClose: 2500,
+    });
+  }, [showNotification]);
+
+  const handleClickLikeButton = useCallback(
+    (e: MouseEvent<HTMLButtonElement>) => {
+      onClick?.(e);
+      openToast();
+    },
+    [onClick, openToast]
+  );
+
+  const icon = useMemo(() => {
+    return (
       <Image width={24} height={24}>
         <Image.Source src="/assets/icon/heart.png" alt="좋아요 아이콘" />
       </Image>
+    );
+  }, []);
+
+  return (
+    <Image.Root as="button" onClick={handleClickLikeButton} {...props}>
+      {icon}
     </Image.Root>
   );
 }
