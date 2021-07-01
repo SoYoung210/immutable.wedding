43fa1b420e039b9626508549980280146b810ac6
SoYoung210 <ethdud1@gmail.com>
2021-07-02 01:33:24 +0900

    feat: main layout옮기고 animate shared layout추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,20 @@ import { NextSeo } from 'next-seo';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import React from 'react';
+import { AnimateSharedLayout } from 'framer-motion';
 import { NotificationProvider } from '@components/notification/NotificationContext';
+import { styled } from 'stitches.config';
+
 import '../styles/globals.css';
+
+const Main = styled('main', {
+  position: 'relative',
+  maxWidth: 520,
+  minWidth: 320,
+  minHeight: '100vh',
+  mx: 'auto',
+  backgroundColor: '$white',
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -13,7 +25,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NotificationProvider>
-        <Component {...pageProps} />
+        <AnimateSharedLayout type="crossfade">
+          <Main>
+            <Component {...pageProps} />
+          </Main>
+        </AnimateSharedLayout>
       </NotificationProvider>
     </>
   );
