11bfb497438b773b540fd2fbd82dad638e7235c3
SoYoung210 <ethdud1@gmail.com>
2021-07-03 16:54:51 +0900

    chore: highlight 스타일 조정

--- src/pages/feeds/components/highlight/Highlight.tsx
+++ src/pages/feeds/components/highlight/Highlight.tsx
@@ -4,23 +4,35 @@ import { Flex } from '@components/util/layout/Flex';
 import { css, styled } from 'stitches.config';
 import { useHighlights } from './useHighlights';
 import Link from 'next/link';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { 스토리_애니메이션_레이아웃 } from '@constants/animationId';
 
 const highlightImageLayout = css({ position: 'relative' });
 
 const StyledMotionLi = styled(motion.li, {
   flex: 'center',
+  flexGrow: 0,
+  flexShrink: 0,
+});
+
+const StyledAnchor = styled('a', {
+  flex: 'center',
   flexDirection: 'column',
   flexGrow: 0,
   flexShrink: 0,
+});
+
+const StyledDiv = styled('div', {
+  position: 'absolute',
+  top: -6,
+  left: -6,
 });
 export function Highlight() {
   const { data: highlights } = useHighlights();
 
   return (
     <Flex
-      elementType="ol"
+      elementType="ul"
       css={{
         spaceX: '$16',
         p: '$20',
@@ -31,35 +43,41 @@ export function Highlight() {
     >
       {highlights.data.map((highlight, index) => {
         return (
-          <Link href={`/stories/${index}`} key={highlight.id}>
+          <Link
+            key={highlight.id}
+            href={{
+              pathname: `/stories/[id]`,
+              query: { id: index },
+            }}
+            passHref={true}
+            shallow={true}
+          >
             <StyledMotionLi layoutId={스토리_애니메이션_레이아웃(index)}>
-              <Image.Root className={highlightImageLayout()}>
-                <Image.RoundShape
-                  width={60}
-                  height={60}
-                  variants={
-                    <div
-                      className={css({
-                        position: 'absolute',
-                        top: -6,
-                        left: -6,
-                      })()}
-                    >
-                      <Gradient.Circle
-                        size={72}
-                        strokeWidth={2.3}
-                        colorKeys={['deepBlue500', 'lightGreen900']}
-                      />
-                    </div>
-                  }
-                >
-                  <Image.Source
-                    src={highlight.profileImage}
-                    alt="스토리_프로필_이미지"
-                  />
-                </Image.RoundShape>
-              </Image.Root>
-              <HighlightName css={{ mt: '$6' }}>{highlight.name}</HighlightName>
+              <StyledAnchor>
+                <Image.Root className={highlightImageLayout()}>
+                  <Image.RoundShape
+                    width={60}
+                    height={60}
+                    variants={
+                      <StyledDiv>
+                        <Gradient.Circle
+                          size={72}
+                          strokeWidth={2.3}
+                          colorKeys={['deepBlue500', 'lightGreen900']}
+                        />
+                      </StyledDiv>
+                    }
+                  >
+                    <Image.Source
+                      src={highlight.profileImage}
+                      alt="스토리_프로필_이미지"
+                    />
+                  </Image.RoundShape>
+                </Image.Root>
+                <HighlightName css={{ mt: '$6' }}>
+                  {highlight.name}
+                </HighlightName>
+              </StyledAnchor>
             </StyledMotionLi>
           </Link>
         );
