820ae4ae6dc9a81b363b440aa02d2cade451b46c
SoYoung210 <ethdud1@gmail.com>
2021-07-05 01:19:00 +0900

    chore: opacity animation

--- src/pages/feeds/components/feed/icon/LikeIcon.tsx
+++ src/pages/feeds/components/feed/icon/LikeIcon.tsx
@@ -55,7 +55,12 @@ export function LikeIcon({ onClick, ...props }: Props) {
       {...props}
     >
       {like ? (
-        <StyledMotionDiv className={iconStyles.likeButton}>
+        <StyledMotionDiv
+          className={iconStyles.likeButton}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.1 }}
+        >
           <FillHeart />
         </StyledMotionDiv>
       ) : (
