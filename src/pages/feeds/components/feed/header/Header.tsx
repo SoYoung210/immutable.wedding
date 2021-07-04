17c411c14a3db1ee91b8d8648cf24adb28a74608
JaeYeopHan <ljyhanll@gmail.com>
2021-07-04 18:40:53 +0900

    fix

--- src/pages/feeds/components/feed/header/Header.tsx
+++ src/pages/feeds/components/feed/header/Header.tsx
@@ -1,25 +1,20 @@
-import cx from 'classnames';
 import Image from '@components/image';
 import Text from '@components/text';
 import { Flex } from '@components/util/layout/Flex';
-import { css } from 'stitches.config';
 import { useAccount } from '../useAccount';
 
-const avatarLayout = css({ display: 'flex' });
 export function Header() {
   const { data: account } = useAccount();
 
   return (
     <Flex.CenterVertical css={{ px: '$12', py: '$8' }}>
       <Image.Root
-        className={cx(
-          avatarLayout(),
-          css({
-            borderRadius: '50%',
-            border: '1px solid $gray200',
-            backgroundColor: '$gray100',
-          })()
-        )}
+        css={{
+          display: 'flex',
+          borderRadius: '50%',
+          border: '1px solid $gray200',
+          backgroundColor: '$gray100',
+        }}
       >
         <Image.RoundShape width={32} height={32}>
           <Image.Source src={account.profileSrc} alt={account.profileAlt} />
