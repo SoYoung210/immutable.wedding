f19622ae029ec9c5523d70a293bd2a2c08380460
SoYoung210 <ethdud1@gmail.com>
2021-07-04 13:14:03 +0900

    feat: footer animation

--- src/pages/feeds/components/feed/footer/Footer.tsx
+++ src/pages/feeds/components/feed/footer/Footer.tsx
@@ -1,15 +1,30 @@
 import { Flex } from '@components/util/layout/Flex';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Text from '@components/text';
 import { useInView } from 'react-intersection-observer';
 import { Share } from './Share';
+import { motion, useAnimation } from 'framer-motion';
 import { CheckIcon } from './CheckIcon';
 
 export function Footer() {
+  const textAnimationControl = useAnimation();
+
   const { ref, inView } = useInView({
     threshold: 0.8,
     triggerOnce: true,
   });
+
+  useEffect(() => {
+    if (inView) {
+      textAnimationControl.start((delay = 0) => {
+        return {
+          opacity: 1,
+          y: 0,
+          transition: { duration: 0.3, delay },
+        };
+      });
+    }
+  }, [textAnimationControl, inView]);
 
   return (
     <div ref={ref}>
@@ -18,23 +33,43 @@ export function Footer() {
         css={{ height: 300, pt: '$40', pb: '$28', boxSizing: 'border-box' }}
       >
         <CheckIcon isStartAnimation={inView} />
-        <Text
-          weight="extralight"
-          size="xxl"
-          css={{ mt: '$20', color: '$warmGray700' }}
+        <motion.div
+          initial={{ opacity: 0, y: 3 }}
+          animate={textAnimationControl}
+          custom={1}
+          transition={{ duration: 0.3 }}
         >
-          끝까지 읽어주셔서 감사합니다
-        </Text>
-
-        <Share />
-        <Text.Anchor
-          weight="extralight"
-          size="sm"
-          css={{ color: '$trueGray500', mt: 'auto' }}
-          href="#"
+          <Text
+            weight="extralight"
+            size="xxl"
+            css={{ mt: '$20', color: '$warmGray700' }}
+          >
+            끝까지 읽어주셔서 감사합니다
+          </Text>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 3 }}
+          animate={textAnimationControl}
+          custom={1.2}
+          transition={{ duration: 0.3 }}
         >
-          맨 위로
-        </Text.Anchor>
+          <Share />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, x: 12 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.3, delay: 1.25 }}
+          style={{ marginTop: 'auto' }}
+        >
+          <Text.Anchor
+            weight="extralight"
+            size="sm"
+            css={{ color: '$trueGray500' }}
+            href="#"
+          >
+            맨 위로
+          </Text.Anchor>
+        </motion.div>
       </Flex.Center>
     </div>
   );
