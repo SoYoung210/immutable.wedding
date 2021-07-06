24616d45b80c0ba78ca7bc4ce5acfcbfb7622ed1
SoYoung210 <ethdud1@gmail.com>
2021-07-06 23:56:52 +0900

    feat: comment animation

--- src/pages/feeds/components/feed/icon/CommentIcon.tsx
+++ src/pages/feeds/components/feed/icon/CommentIcon.tsx
@@ -3,11 +3,14 @@ import { useNotifications } from '@components/notification/NotificationContext';
 import React, { HTMLAttributes, MouseEvent, useCallback } from 'react';
 import { ToastWrapper } from '../ToastWrapper';
 import { Comment } from '@components/icon/Comment';
+import { motion, useAnimation } from 'framer-motion';
 
 type Props = HTMLAttributes<HTMLButtonElement>;
 
+const 한바퀴_회전_각도 = 360;
 export function CommentIcon({ onClick, ...props }: Props) {
   const { showNotification } = useNotifications();
+  const commentAnimationControl = useAnimation();
 
   const openToast = useCallback(() => {
     showNotification({
@@ -15,12 +18,30 @@ export function CommentIcon({ onClick, ...props }: Props) {
     });
   }, [showNotification]);
 
+  const animateSequence = useCallback(async () => {
+    commentAnimationControl.stop();
+    await commentAnimationControl.start({
+      rotate: [0, 한바퀴_회전_각도],
+      transition: {
+        duration: 0.6,
+      },
+    });
+    commentAnimationControl.start({
+      scale: [1, 0.3, 1],
+      rotate: [0, 한바퀴_회전_각도 * 2],
+      transition: {
+        duration: 1.3,
+      },
+    });
+  }, [commentAnimationControl]);
+
   const handleClickLikeButton = useCallback(
     (e: MouseEvent<HTMLButtonElement>) => {
       onClick?.(e);
       openToast();
+      animateSequence();
     },
-    [onClick, openToast]
+    [animateSequence, onClick, openToast]
   );
 
   return (
@@ -30,7 +51,9 @@ export function CommentIcon({ onClick, ...props }: Props) {
       onClick={handleClickLikeButton}
       {...props}
     >
-      <Comment />
+      <motion.div animate={commentAnimationControl}>
+        <Comment />
+      </motion.div>
     </Image.Root>
   );
 }
