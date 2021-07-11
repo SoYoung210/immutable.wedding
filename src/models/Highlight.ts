0dcf0956fc057fef804df83d92cbbe859db0716c
JaeYeopHan <ljyhanll@gmail.com>
2021-07-11 16:47:33 +0900

    feat: define Highlight model

---
+++ src/models/Highlight.ts
@@ -0,0 +1,19 @@
+interface Image {
+  blurDataURL: string;
+  src: string;
+  height: number;
+  width: number;
+  type?: string;
+}
+
+interface HighlightContent {
+  id: number;
+  image: Image;
+}
+
+export interface Highlight {
+  id: number;
+  name: string;
+  profileImage: Image;
+  contents: HighlightContent[];
+}
