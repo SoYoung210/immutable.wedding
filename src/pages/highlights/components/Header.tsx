5d409b21a4090ff2bfaf299c201511b1ba2e9a8c
SoYoung210 <ethdud1@gmail.com>
2021-07-17 16:29:02 +0900

    feat: highlight header layout

--- src/pages/highlights/components/Header.tsx
+++ src/pages/highlights/components/Header.tsx
@@ -1,9 +1,10 @@
 import { NextImage } from '@models/common/NextImage';
 import { motion } from 'framer-motion';
-import { useRouter } from 'next/router';
 import React, { PropsWithChildren } from 'react';
 import Image from '@components/image';
+import CloseIcon from '@components/icon/Close';
 import { Flex } from '@components/util/layout/Flex';
+import { styled } from 'stitches.config';
 
 interface Props {
   thumbnailImage: NextImage;
@@ -15,24 +16,28 @@ export function Header({
   onClose,
   children,
 }: PropsWithChildren<Props>) {
-  const router = useRouter();
-  const { id } = router.query;
-
   return (
-    <Flex.CenterVertical css={{ padding: '$12' }}>
-      <motion.div
-        style={{ marginRight: '6px' }}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.3 }}
-      >
-        <Image.Root>
+    <motion.div
+      initial={{ y: 10, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.4 }}
+    >
+      <Flex.CenterVertical css={{ py: '$12', px: '$18' }}>
+        <Image.Root css={{ mr: '$6' }}>
           <Image.RoundShape {...thumbnailImage} width={30} height={30}>
             <Image.Source src={thumbnailImage.src} alt="재여비" />
           </Image.RoundShape>
         </Image.Root>
-      </motion.div>
-      {children}
-    </Flex.CenterVertical>
+
+        {children}
+        <SButton type="button" onClick={onClose}>
+          <CloseIcon />
+        </SButton>
+      </Flex.CenterVertical>
+    </motion.div>
   );
 }
+
+const SButton = styled('button', {
+  ml: 'auto',
+});
