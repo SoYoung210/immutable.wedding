5220db038961c10b81292fe902b824418e7671b0
JaeYeopHan <ljyhanll@gmail.com>
2021-07-18 17:51:51 +0900

    feat: apply animation to bottom sheet

--- src/components/bottom-sheet/BottonSheet.tsx
+++ src/components/bottom-sheet/BottonSheet.tsx
@@ -1,5 +1,7 @@
 import { Flex } from '@components/util/layout/Flex';
+import { fadeInOut } from '@motion/fadeInOut';
 import { PortalConsumer } from '@providers/PortalProvider';
+import { AnimatePresence, motion } from 'framer-motion';
 import React, { MouseEvent, ReactNode, useCallback } from 'react';
 import { styled } from 'stitches.config';
 
@@ -31,35 +33,48 @@ export function BottomSheet({
 
   return (
     <PortalConsumer>
-      <Fixed css={{ display: open ? 'block' : 'none' }}>
-        <Dimmer onClick={handleDimmerClick}>
-          <BottomSheetWrapper direction="column">
-            <Flex justify="between" css={{ width: '100%' }}>
-              <Flex direction="column">
-                <div>{title}</div>
-                <div>{description}</div>
+      <AnimatePresence>
+        {open ? (
+          <Dimmer
+            onClick={handleDimmerClick}
+            variants={fadeInOut.Variants}
+            initial={fadeInOut.HIDDEN}
+            animate={fadeInOut.VISIBLE}
+            exit={fadeInOut.HIDDEN}
+            transition={{
+              type: 'spring',
+              duration: 0.3,
+            }}
+          >
+            <BottomSheetWrapper
+              initial={{ y: 120 }}
+              animate={{ y: 0 }}
+              exit={{ y: 120 }}
+              transition={{
+                type: 'spring',
+                bounce: 0,
+                duration: 0.2,
+              }}
+            >
+              <Flex justify="between" css={{ width: '100%' }}>
+                <Flex direction="column">
+                  <div>{title}</div>
+                  <div>{description}</div>
+                </Flex>
+                {rightAddon}
               </Flex>
-              {rightAddon}
-            </Flex>
-            <div>{children}</div>
-          </BottomSheetWrapper>
-        </Dimmer>
-      </Fixed>
+              <div>{children}</div>
+            </BottomSheetWrapper>
+          </Dimmer>
+        ) : null}
+      </AnimatePresence>
     </PortalConsumer>
   );
 }
 
-const Fixed = styled('div', {
-  position: 'absolute',
-  top: 0,
-  bottom: 0,
-  left: 0,
-  right: 0,
-});
-
-const Dimmer = styled('div', {
+const Dimmer = styled(motion.div, {
   content: ' ',
-  position: 'absolute',
+  position: 'fixed',
   top: 0,
   right: 0,
   bottom: 0,
@@ -68,7 +83,10 @@ const Dimmer = styled('div', {
   backgroundColor: 'rgba(0, 0, 0, 0.7)',
 });
 
-const BottomSheetWrapper = styled(Flex, {
+const BottomSheetWrapper = styled(motion.div, {
+  display: 'flex',
+  flexDirection: 'column',
+
   position: 'absolute',
   bottom: 0,
   margin: '0 auto',
