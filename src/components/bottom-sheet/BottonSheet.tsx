eac8f8ce34f56673eccea160d83bda459992d2c7
JaeYeopHan <ljyhanll@gmail.com>
2021-07-18 18:30:48 +0900

    feat: BottomSheet title, description

--- src/components/bottom-sheet/BottonSheet.tsx
+++ src/components/bottom-sheet/BottonSheet.tsx
@@ -2,10 +2,11 @@ import { Dimmer } from '@components/dimmer/Dimmer';
 import { Flex } from '@components/util/layout/Flex';
 import { PortalConsumer } from '@providers/PortalProvider';
 import { AnimatePresence, motion } from 'framer-motion';
-import React, { ReactNode } from 'react';
+import React, { isValidElement, ReactNode } from 'react';
 import { styled } from 'stitches.config';
 import { fadeInOut } from '@motion/fadeInOut';
 import { slideUpDown } from '@motion/slideUpDown';
+import CloseIcon from '@components/icon/Close';
 
 interface Props {
   open: boolean;
@@ -30,14 +31,31 @@ export function BottomSheet({
         {open ? (
           <Dimmer onDimmerClick={onClose} {...fadeInOut()}>
             <BottomSheetWrapper {...slideUpDown()}>
-              <Flex justify="between" css={{ width: '100%' }}>
+              <Flex
+                justify="between"
+                css={{ width: '100%', marginBottom: '$8' }}
+              >
                 <Flex direction="column">
-                  <div>{title}</div>
-                  <div>{description}</div>
+                  {isValidElement(title) ? (
+                    title
+                  ) : (
+                    <BottomSheet.Title>{title}</BottomSheet.Title>
+                  )}
+                  {isValidElement(description) ? (
+                    description
+                  ) : (
+                    <BottomSheet.Description>
+                      {description}
+                    </BottomSheet.Description>
+                  )}
                 </Flex>
-                {rightAddon}
+                {isValidElement(rightAddon) ? (
+                  rightAddon
+                ) : (
+                  <BottomSheet.CloseButton onClick={onClose} />
+                )}
               </Flex>
-              <div>{children}</div>
+              {children}
             </BottomSheetWrapper>
           </Dimmer>
         ) : null}
@@ -45,6 +63,28 @@ export function BottomSheet({
     </PortalConsumer>
   );
 }
+
+BottomSheet.Title = styled('div', {
+  color: '$gray900',
+  fontWeight: '$bold',
+  fontSize: '$xxl',
+  marginBottom: 4,
+});
+
+BottomSheet.Description = styled('div', {
+  color: '$gray500',
+  fontWeight: '$medium',
+  fontSize: '$lg',
+  letterSpacing: -1,
+});
+
+BottomSheet.CloseButton = ({ onClick }: { onClick: () => void }) => {
+  return (
+    <button onClick={onClick}>
+      <CloseIcon />
+    </button>
+  );
+};
 
 const BottomSheetWrapper = styled(motion.div, {
   display: 'flex',
