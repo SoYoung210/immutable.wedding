6c2d2813ae2ad41e763f9bf65e601462910f1655
JaeYeopHan <ljyhanll@gmail.com>
2021-07-25 21:23:04 +0900

    fix: width default value

--- src/components/list/ListItemImage.tsx
+++ src/components/list/ListItemImage.tsx
@@ -2,6 +2,6 @@ import { HTMLAttributes, HTMLProps } from 'react';
 
 type Props = HTMLProps<HTMLImageElement>;
 
-export function ListItemImage(props: Props) {
-  return <img {...(props as HTMLAttributes<HTMLImageElement>)} />;
+export function ListItemImage({ width = 36, ...props }: Props) {
+  return <img {...(props as HTMLAttributes<HTMLImageElement>)} width={width} />;
 }
