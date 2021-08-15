fdd7f2f33ffb0a9d4784b378234016db81287fe1
SoYoung210 <ethdud1@gmail.com>
2021-08-15 22:32:20 +0900

    feat: add highlihgt blur bg

--- pages/highlights/[id].tsx
+++ pages/highlights/[id].tsx
@@ -6,6 +6,9 @@ import { useRouter } from 'next/router';
 import { getPlaiceholder } from 'plaiceholder';
 import React from 'react';
 import { Header } from '@pages/highlights/components/Header';
+import { useAccount } from '@hooks/data/useAccount';
+
+import { styled } from 'stitches.config';
 
 async function fetchHighlights() {
   const highlightJson = (await import('public/assets/data/highlights.json'))
@@ -57,6 +60,7 @@ type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
 export default function HighlightPage({ highlight }: Props) {
   const router = useRouter();
+  const { data: account } = useAccount();
 
   if (highlight == null) {
     return <div>잘못된 접근입니다</div>;
@@ -65,22 +69,46 @@ export default function HighlightPage({ highlight }: Props) {
   const 대표_컨텐츠_이미지 = highlight.contents[0].image;
 
   return (
-    <>
-      <Header thumbnailImage={highlight.thumbnailImage} onClose={router.back}>
-        immutable.wedding(soso, jbee)
-      </Header>
-      <motion.div
-        style={{ marginRight: '6px' }}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.3 }}
+    <Wrapper>
+      <Content
+        style={{
+          backgroundImage: `url(${대표_컨텐츠_이미지.blurDataURL})`,
+        }}
       >
-        <Image.Root>
-          <Image {...대표_컨텐츠_이미지} width={520} height={520}>
-            <Image.Source src={대표_컨텐츠_이미지.src} alt="재여비" />
-          </Image>
-        </Image.Root>
-      </motion.div>
-    </>
+        <Header thumbnailImage={highlight.thumbnailImage} onClose={router.back}>
+          {account.name}
+        </Header>
+        <StyledMotionDiv
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.3 }}
+        >
+          <Image.Root>
+            <Image {...대표_컨텐츠_이미지} width={520} height={520}>
+              <Image.Source src={대표_컨텐츠_이미지.src} alt="재여비" />
+            </Image>
+          </Image.Root>
+          {/* <CountdownBox /> */}
+        </StyledMotionDiv>
+      </Content>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled('section', {
+  backgroundColor: '$black',
+  height: '100vh',
+});
+
+const Content = styled('div', {
+  br: '$3',
+  height: '100%',
+  position: 'relative',
+  backgroundSize: 'cover',
+});
+
+const StyledMotionDiv = styled(motion.div, {
+  position: 'absolute',
+  top: '50%',
+  transform: 'translateY(-50%)',
+});
