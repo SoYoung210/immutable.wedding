c68228919bfe231de2a44b605e847406ae08de7e
JaeYeopHan <ljyhanll@gmail.com>
2021-08-16 18:54:11 +0900

    fix: suspense 적용해서 nullable 로직 제거

--- src/pages/feeds/components/feed/comments/useComments.ts
+++ src/pages/feeds/components/feed/comments/useComments.ts
@@ -1,6 +1,16 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { getComments } from '@remotes/comments';
 import useSWR from 'swr';
 
 export function useComments(id: number) {
-  return useSWR([id, 'getComments'], getComments);
+  const { data, ...rest } = useSWR([id, 'getComments'], getComments, {
+    suspense: true,
+  });
+
+  return {
+    data: data!,
+    isEmpty: data!.length === 0,
+    hasOnlyOne: data!.length - 1 === 0,
+    ...rest,
+  };
 }
