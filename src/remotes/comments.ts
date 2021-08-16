685b9a2a36bbf9f5f13edf43a52d42178def2f39
Jbee <ljyhanll@gmail.com>
2021-08-16 20:35:03 +0900

    feat: 댓글 부분 버그 수정 및 UX 향상 (#46)
    * fix: id도 select 하도록 수정
    
    * fix: suspense 적용해서 nullable 로직 제거
    
    * feat: 댓글남기기 UX 향상

--- src/remotes/comments.ts
+++ src/remotes/comments.ts
@@ -1,28 +1,28 @@
 import { api } from '@remotes/supabaseClient';
 
-interface Comment {
+export interface Comment {
   id: number;
   feedId: number;
   message: string;
   createAt: string;
 }
 
-export async function getComments(id: number) {
+export async function getComments(feedId: number) {
   const { data } = await api
     .from<Comment>('comments')
-    .select('feedId, message, createAt')
-    .eq('feedId', id)
+    .select('id, feedId, message, createAt')
+    .eq('feedId', feedId)
     .order('id', { ascending: false });
 
   return data;
 }
 
 export async function addComment(
-  id: number,
+  feedId: number,
   { message }: Pick<Comment, 'message'>
 ) {
   return api.from<Comment>('comments').upsert({
-    feedId: id,
+    feedId,
     message,
     createAt: new Date().toISOString(),
   });
