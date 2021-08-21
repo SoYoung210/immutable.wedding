ea306186f0451003e844a507d1489ef0ab5ec2b4
SoYoung210 <ethdud1@gmail.com>
2021-08-21 22:56:15 +0900

    chore: remove header transition

--- src/pages/highlights/components/Header.tsx
+++ src/pages/highlights/components/Header.tsx
@@ -5,7 +5,6 @@ import Image from '@components/image';
 import CloseIcon from '@components/icon/Close';
 import { Flex } from '@components/util/layout/Flex';
 import { styled } from 'stitches.config';
-import fadeEntrance from '@utils/animation/fadeEntrance';
 
 interface Props {
   thumbnailImage: NextImage;
@@ -18,14 +17,11 @@ export function Header({
   children,
 }: PropsWithChildren<Props>) {
   return (
-    <motion.div
-      {...fadeEntrance.y({ opacityTransition: false })}
-      transition={{ duration: 0.4 }}
-    >
+    <header>
       <Flex.CenterVertical css={{ py: '$12', px: '$18' }}>
         <SImageRoot>
           <Image.RoundShape {...thumbnailImage} width={30} height={30}>
-            <Image.Source src={thumbnailImage.src} alt="재여비" />
+            <Image.Source src={thumbnailImage.src} alt="프로필_사진" />
           </Image.RoundShape>
         </SImageRoot>
 
@@ -34,7 +30,7 @@ export function Header({
           <CloseIcon />
         </SButton>
       </Flex.CenterVertical>
-    </motion.div>
+    </header>
   );
 }
 
