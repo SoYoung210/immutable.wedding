33eb558de641475fe4a13d262b2e98394426dfa3
SoYoung210 <ethdud1@gmail.com>
2021-08-21 18:57:05 +0900

    refactor: ContentWrapper 분리

--- pages/highlights/[id].tsx
+++ pages/highlights/[id].tsx
@@ -1,12 +1,6 @@
 import Image from '@components/image';
 import { Highlight, RawHighlightData } from '@models/Highlight';
-import {
-  AnimatePresence,
-  motion,
-  PanInfo,
-  useMotionValue,
-  useTransform,
-} from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import { GetStaticPropsContext, InferGetStaticPropsType } from 'next';
 import { useRouter } from 'next/router';
 import { getPlaiceholder } from 'plaiceholder';
@@ -15,7 +9,8 @@ import { Header } from '@pages/highlights/components/Header';
 import { useAccount } from '@hooks/data/useAccount';
 
 import { styled } from 'stitches.config';
-import React, { useLayoutEffect, useState } from 'react';
+import React, { useCallback, useLayoutEffect, useState } from 'react';
+import ContentWrapper from '@pages/highlights/components/ContentWrapper';
 
 async function fetchHighlights() {
   const highlightJson = (await import('public/assets/data/highlights.json'))
@@ -78,57 +73,6 @@ export async function getStaticProps({ params }: GetStaticPropsContext) {
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
-function Content2(props: any) {
-  const x = useMotionValue(0);
-  const scale = useTransform(x, [-150, 0, 150], [0.5, 1, 0.5]);
-  function handleDragEnd(_, info: PanInfo) {
-    if (info.offset.x < -100) {
-      props.setExitX('-250px');
-      props.setIndex(props.index + 1);
-    }
-    if (info.offset.x > 100) {
-      props.setExitX('250px');
-      props.setIndex(props.index - 1);
-    }
-  }
-
-  return (
-    <StyledMotionWrapper
-      style={{
-        x,
-        width: '100%',
-        cursor: 'grab',
-      }}
-      drag={props.drag}
-      dragConstraints={{
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0,
-      }}
-      onDragEnd={handleDragEnd}
-      whileTap={{ cursor: 'grabbing' }}
-      initial={props.initial}
-      animate={props.animate}
-      transition={props.transition}
-      exit={{
-        x: props.exitX,
-        opacity: 0,
-        scale: 0.5,
-        transition: { duration: 0.2 },
-      }}
-    >
-      <BackgroundMotionDiv
-        style={{
-          scale,
-          backgroundImage: `url(${props.대표_컨텐츠_이미지.blurDataURL})`,
-        }}
-      >
-        {props.children}
-      </BackgroundMotionDiv>
-    </StyledMotionWrapper>
-  );
-}
 export default function HighlightPage({
   highlight,
   highlighIds,
@@ -140,12 +84,18 @@ export default function HighlightPage({
       highlightDataSet?.findIndex(dataSet => dataSet.id === highlight?.id) ?? 0
     );
   });
-  const [exitX, setExitX] = useState('100%');
+
+  const setNext = useCallback(() => {
+    setIndex(prev => prev + 1);
+  }, []);
+
+  const setPrev = useCallback(() => {
+    setIndex(prev => prev + 1);
+  }, []);
 
   const { data: account } = useAccount();
 
   useLayoutEffect(() => {
-    console.log('indx', index);
     if (index > highlightDataSet!.length || index < 0) {
       router.push('/');
     }
@@ -154,20 +104,23 @@ export default function HighlightPage({
   if (highlight == null || highlighIds == null || highlightDataSet == null) {
     return <div>잘못된 접근입니다</div>;
   }
+  const 유효한_범위인가 = index > -1 && index < highlightDataSet.length;
   const 이전_컨텐츠_대표_이미지 =
     index > 0 ? highlightDataSet[index - 1].contents[0].image : null;
   const 다음_컨텐츠_대표_이미지 =
     index < highlightDataSet.length - 1
       ? highlightDataSet[index + 1].contents[0].image
       : null;
-  const 대표_컨텐츠_이미지 = highlightDataSet[index].contents[0].image;
+  const 대표_컨텐츠_이미지 = 유효한_범위인가
+    ? highlightDataSet[index].contents[0].image
+    : null;
 
   return (
     <AnimatePresence initial={false}>
       {다음_컨텐츠_대표_이미지 != null ? (
-        <Content2
+        <ContentWrapper
           key={index + 1}
-          대표_컨텐츠_이미지={다음_컨텐츠_대표_이미지}
+          imageContent={다음_컨텐츠_대표_이미지}
           initial={{ scale: 0, y: 105, opacity: 0 }}
           animate={{ scale: 0.75, y: 30, opacity: 0.5 }}
           transition={{
@@ -192,58 +145,43 @@ export default function HighlightPage({
               </Image>
             </Image.Root>
           </StyledMotionDiv>
-        </Content2>
+        </ContentWrapper>
       ) : null}
 
-      <Content2
-        key={index}
-        대표_컨텐츠_이미지={대표_컨텐츠_이미지}
-        animate={{ scale: 1, y: 0, opacity: 1 }}
-        drag="x"
-        transition={{
-          type: 'spring',
-          stiffness: 300,
-          damping: 20,
-          opacity: { duration: 0.2 },
-        }}
-        exitX={exitX}
-        setExitX={setExitX}
-        index={index}
-        setIndex={setIndex}
-      >
-        <Header thumbnailImage={highlight.thumbnailImage} onClose={router.back}>
-          {account.name}
-        </Header>
-        <StyledMotionDiv
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.3 }}
+      {대표_컨텐츠_이미지 != null ? (
+        <ContentWrapper
+          key={index}
+          imageContent={대표_컨텐츠_이미지}
+          animate={{ scale: 1, y: 0, opacity: 1 }}
+          drag="x"
+          setPrev={setPrev}
+          setNext={setNext}
+          transition={{
+            opacity: { duration: 0.2 },
+          }}
         >
-          <Image.Root>
-            <Image {...대표_컨텐츠_이미지} width={520} height={520}>
-              <Image.Source src={대표_컨텐츠_이미지.src} alt="재여비" />
-            </Image>
-          </Image.Root>
-        </StyledMotionDiv>
-      </Content2>
+          <Header
+            thumbnailImage={highlight.thumbnailImage}
+            onClose={router.back}
+          >
+            {account.name}
+          </Header>
+          <StyledMotionDiv
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.3 }}
+          >
+            <Image.Root>
+              <Image {...대표_컨텐츠_이미지} width={520} height={520}>
+                <Image.Source src={대표_컨텐츠_이미지.src} alt="재여비" />
+              </Image>
+            </Image.Root>
+          </StyledMotionDiv>
+        </ContentWrapper>
+      ) : null}
     </AnimatePresence>
   );
 }
-
-const StyledMotionWrapper = styled(motion.section, {
-  backgroundColor: '$transparent',
-  height: '100vh',
-
-  position: 'absolute',
-  top: 0,
-});
-
-const BackgroundMotionDiv = styled(motion.div, {
-  height: '100%',
-  br: '$3',
-
-  backgroundSize: 'cover',
-});
 
 const StyledMotionDiv = styled(motion.div, {
   position: 'absolute',
---
+++ src/pages/highlights/components/ContentWrapper.tsx
@@ -0,0 +1,107 @@
+import { NextImage } from '@models/common/NextImage';
+import {
+  DraggableProps,
+  motion,
+  MotionProps,
+  PanInfo,
+  useMotionValue,
+  useTransform,
+} from 'framer-motion';
+import { PropsWithChildren, useEffect, useState } from 'react';
+import { styled } from 'stitches.config';
+
+interface Props extends DraggableProps, MotionProps {
+  imageContent: NextImage | null;
+  setNext?: () => void;
+  setPrev?: () => void;
+}
+
+const FADE_OUT = {
+  LEFT: -250,
+  RIGHT: 250,
+};
+
+export default function ContentWrapper({
+  imageContent,
+  setNext,
+  setPrev,
+  children,
+  ...props
+}: PropsWithChildren<Props>) {
+  const x = useMotionValue(0);
+  const scale = useTransform(x, [-150, 0, 150], [0.5, 1, 0.5]);
+  const [exitX, setExitX] = useState<string | number>('100%');
+
+  useEffect(() => {
+    if (exitX === FADE_OUT.LEFT) {
+      setNext?.();
+    }
+
+    if (exitX === FADE_OUT.RIGHT) {
+      setPrev?.();
+    }
+  }, [exitX, setNext, setPrev]);
+
+  function handleDragEnd(
+    _: MouseEvent | TouchEvent | PointerEvent,
+    info: PanInfo
+  ) {
+    if (info.offset.x < -100) {
+      setExitX(FADE_OUT.LEFT);
+    }
+    if (info.offset.x > 100) {
+      setExitX(FADE_OUT.RIGHT);
+    }
+  }
+
+  return (
+    <StyledMotionWrapper
+      style={{
+        x,
+        width: '100%',
+        cursor: 'grab',
+      }}
+      dragConstraints={{
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+      }}
+      onDragEnd={handleDragEnd}
+      whileTap={{ cursor: 'grabbing' }}
+      exit={{
+        x: exitX,
+        opacity: 0,
+        scale: 0.5,
+        transition: { duration: 0.2 },
+      }}
+      {...props}
+    >
+      {imageContent != null ? (
+        <BackgroundMotionDiv
+          style={{
+            scale,
+            backgroundImage: `url(${imageContent.blurDataURL})`,
+          }}
+        >
+          {children}
+        </BackgroundMotionDiv>
+      ) : null}
+    </StyledMotionWrapper>
+  );
+}
+
+const StyledMotionWrapper = styled(motion.section, {
+  backgroundColor: '$transparent',
+  height: '100vh',
+
+  position: 'absolute',
+  top: 0,
+});
+
+const BackgroundMotionDiv = styled(motion.div, {
+  height: '100%',
+  br: '$3',
+
+  backgroundSize: 'cover',
+});
