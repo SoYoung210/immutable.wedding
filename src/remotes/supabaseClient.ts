5b64a65ffcff374433531fd191ffe61268ae475e
JaeYeopHan <ljyhanll@gmail.com>
2021-09-05 17:17:12 +0900

    fix: wedding.log 전용으로 db key 값 수정

--- src/remotes/supabaseClient.ts
+++ src/remotes/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_WEDDING_LOG_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_WEDDING_LOG_ANON_KEY;
 
 if (supabaseUrl == null || supabaseAnonKey == null) {
   throw Error(
